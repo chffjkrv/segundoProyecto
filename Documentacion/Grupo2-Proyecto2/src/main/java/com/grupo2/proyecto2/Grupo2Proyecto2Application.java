758ab674bbf2ad30eb628e3169df77fd918ad3d8
Mohibul <moihbul@moihbul.com>
2019-08-27 14:39:38 +0200

    carcasa del proyecto

---
+++ Documentacion/Grupo2-Proyecto2/src/main/java/com/grupo2/proyecto2/Grupo2Proyecto2Application.java
@@ -0,0 +1,13 @@
+package com.grupo2.proyecto2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo2Proyecto2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo2Proyecto2Application.class, args);
+	}
+
+}
