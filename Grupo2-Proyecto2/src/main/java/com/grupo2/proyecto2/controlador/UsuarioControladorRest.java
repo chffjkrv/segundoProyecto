0d474ae199023840e37fbf89ba17c0ad25081c34
Juan Alfonso <juanalfonso111@gmail.com>
2019-09-04 13:55:46 +0200

    Clase RestUsuarioControlador completo

--- Grupo2-Proyecto2/src/main/java/com/grupo2/proyecto2/controlador/UsuarioControladorRest.java
+++ Grupo2-Proyecto2/src/main/java/com/grupo2/proyecto2/controlador/UsuarioControladorRest.java
@@ -3,23 +3,25 @@ package com.grupo2.proyecto2.controlador;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.grupo2.proyecto2.modelo.Contacto;
 import com.grupo2.proyecto2.modelo.Usuario;
-import com.grupo2.proyecto2.repositorio.IContactoRepositorio;
-import com.grupo2.proyecto2.repositorio.IDescartesRepositorio;
 import com.grupo2.proyecto2.repositorio.IUsuarioRepositorio;
 import com.grupo2.proyecto2.servicios.IServicios;
 
+/**
+ * Nombre de clase. UsuarioControlador con REST.
+ * Descripcion. Capa encargada de gestionar el flujo de datos para ofrecerselos al front.
+ * @version 2.0
+ * @author grupo2
+ * @since 04-09-2019
+ */
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping({ "/users" })
@@ -29,14 +31,15 @@ public class UsuarioControladorRest {
 	private IServicios servicios;
 	@Autowired
 	private IUsuarioRepositorio repo;
-	@Autowired
-	private IContactoRepositorio repocontacto;
-	@Autowired
-	private IDescartesRepositorio repodescartes;
 
-	@PostMapping (path = { "/adduser" })
+	@PostMapping(path = { "/adduser" })
 	public Usuario crearUsuario(@RequestBody Usuario user) {
 		return servicios.crearoEditarUsuarioService(user);
+	}
+
+	@GetMapping(path = { "/login/{id}" })
+	public Usuario login(@PathVariable("id") int id) {
+		return servicios.geUsuarioById(id);
 	}
 
 	@GetMapping // (path = { "/{id}" })
@@ -49,44 +52,39 @@ public class UsuarioControladorRest {
 		return usu;
 	}
 
-//		@PutMapping(path = { "/{id}" })
-//		public Usuario update(@PathVariable("id") int id, @RequestBody Usuario user) {
-//			user.setId(id);
-//			return  servicios(user);
-//		}
+	@GetMapping(path = { "/listall" })
+	public List<Usuario> findAll() {
+		return repo.findAll();
+	}
 
-//		@DeleteMapping(path = { "/{id}" })
-//		public Usuario delete(@PathVariable("id") int id) {
-//			return  servicios.deleteById(id);
-//		}
+	@GetMapping(path = { "/darlike/{id1}/{id2}" })
+	public void darLike(@PathVariable("id1") int id1, @PathVariable("id2") int id2) {
 
-		@GetMapping (path = { "/listall" })
-		public List<Usuario> findAll() {
-			return  repo.findAll();
-		}
-		
-		@GetMapping(path= {"/darlike/{id1}/{id2}"})
-		public void darLike(@PathVariable("id1") int id1, @PathVariable("id2") int id2){
-			
-			servicios.crearContactoService(id1, id2);		
-			
-		}
-		
-		@GetMapping(path= {"/dardislike/{id1}/{id2}"})
-		public void darDisike(@PathVariable("id1") int id1, @PathVariable("id2") int id2){
-			
-			servicios.crearDescarteService(id1, id2);		
-			
-		}
-		
-		@GetMapping(path = { "/listardescartes/{id}" })
-	    public List<Usuario> listarDescartes (@PathVariable("id")int id){//(@PathVariable("id") int id){       //(@RequestBody Usuario user){
-			
-	        List <Usuario>  descartes = servicios.listarDescartes(id);
-	       // System.out.println(user.toString());
-	        
-	    
-	        return descartes;
-		}
+		servicios.crearContactoService(id1, id2);
+
+	}
+
+	@GetMapping(path = { "/dardislike/{id1}/{id2}" })
+	public void darDisike(@PathVariable("id1") int id1, @PathVariable("id2") int id2) {
+
+		servicios.crearDescarteService(id1, id2);
+
+	}
+
+	@GetMapping(path = { "/listardescartes/{id}" })
+	public List<Usuario> listarDescartes(@PathVariable("id") int id) {
+
+		List<Usuario> descartes = servicios.listarDescartes(id);
+
+		return descartes;
+	}
+
+	@GetMapping(path = { "/listarcontactos/{id}" })
+	public List<Usuario> listarContactos(@PathVariable("id") int id) {
+
+		List<Usuario> contactos = servicios.listarContactos(id);
+
+		return contactos;
+	}
 
 }
